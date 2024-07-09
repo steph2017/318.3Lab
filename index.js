63da7b81bf7c84049c4bf344b518ec26eeafb9fe
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-07-09 13:46:41 -0400

    reorganized content  into routers and added a logging middleware

--- index.js
+++ index.js
@@ -2,34 +2,43 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-// Importing the data from our fake database files.
-const users = require("./data/users");
-const posts = require("./data/posts");
+// We import the body-parser package.
+// This package contains middleware that can handle
+// the parsing of many different kinds of data,
+// making it easier to work with data in routes that
+// accept data from the client (POST, PATCH).
+const bodyParser = require("body-parser");
+
+// These are now route imports, not database imports!
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+
+// We use the body-parser middleware FIRST so that
+// we have access to the parsed data within our routes.
+// The parsed data will be located in "req.body".
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
 
-app.get("/", (req, res) => {
-    res.send("Work in progress!");
+// Logging Middlewaare
+app.use((req, res, next) => {
+    const time = new Date();
+
+    console.log(
+        `-----
+  ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+    );
+    if (Object.keys(req.body).length > 0) {
+        console.log("Containing the data:");
+        console.log(`${JSON.stringify(req.body)}`);
+    }
+    next();
 });
 
-app.get("/api/users", (req, res) => {
-    res.json(users);
-});
 
-app.get("/api/users/:id", (req, res, next) => {
-    const user = users.find((u) => u.id == req.params.id);
-    if (user) res.json(user);
-    else next();
-});
-
-app.get("/api/posts", (req, res) => {
-    res.json(posts);
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-    const post = posts.find((p) => p.id == req.params.id);
-    if (post) res.json(post);
-    else next();
-});
+// Use our Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 app.get("/", (req, res) => {
     res.send("Work in progress!");
